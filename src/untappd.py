77afd812575b95f2388669b63d931c61ef12bf79
Ben Smith <bh2smith@gmail.com>
2023-06-03 11:20:21 +0200

    cleanup

--- src/untappd.py
+++ src/untappd.py
@@ -5,16 +5,19 @@ from requests.auth import HTTPBasicAuth, AuthBase
 from dotenv import load_dotenv
 
 
-def try_fetch(menu_id: str, auth: AuthBase):
+def try_fetch(menu_id: int, auth: AuthBase):
     response = requests.get(
-        url=f"https://business.untappd.com/api/v1/items/{menu_id}", auth=auth
+        url=f"https://business.untappd.com/api/v1/items/{menu_id}", 
+        auth=auth
     )
     print(menu_id, response.json())
 
 
 if __name__ == "__main__":
     load_dotenv()
-    basic_auth = HTTPBasicAuth(os.environ["UNTAPPD_USER"], os.environ["UNTAPPD_PASS"])
-    for i in range(100):
-        print(str(i).rjust(2, "0"))
-        try_fetch("304585" + str(i).rjust(2, "0"), auth=basic_auth)
+    start = 30458500
+    for i in range(start, start + 100):
+        try_fetch(
+            menu_id=i, 
+            auth=HTTPBasicAuth(os.environ["UNTAPPD_USER"], os.environ["UNTAPPD_PASS"])
+        )
