23f22f75de719a25aa167301a7d8773f31560863
Ben Smith <bh2smith@gmail.com>
2023-06-03 14:16:44 +0200

    black:fmt

--- src/untappd.py
+++ src/untappd.py
@@ -7,8 +7,7 @@ from dotenv import load_dotenv
 
 def try_fetch(menu_id: int, auth: AuthBase):
     response = requests.get(
-        url=f"https://business.untappd.com/api/v1/items/{menu_id}", 
-        auth=auth
+        url=f"https://business.untappd.com/api/v1/items/{menu_id}", auth=auth
     )
     print(menu_id, response.json())
 
@@ -18,6 +17,9 @@ if __name__ == "__main__":
     start = 30458500
     for i in range(start, start + 100):
         try_fetch(
-            menu_id=i, 
-            auth=HTTPBasicAuth(os.environ["UNTAPPD_USER"], os.environ["UNTAPPD_PASS"])
+            menu_id=i,
+            auth=HTTPBasicAuth(
+                username=os.environ["UNTAPPD_USER"],
+                password=os.environ["UNTAPPD_PASS"],
+            ),
         )
